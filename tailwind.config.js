41b1b73d7f3b83c1a85197026c609defcc8695b7
Ochi Daniel <dtonye338@gmail.com>
2023-01-15 21:13:24 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '760px',
+      lg: '976px',
+      xl: '1440px'
+    },
+    extend: {
+      colors: {
+        brightRed: 'hsl(12, 88%, 59%)',
+        bronzeThick:  '#B97B1B',
+        bronzeLight: '#CA8F34'
+
+      },
+    },
+  },
+  plugins: [],
+}
